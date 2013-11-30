3e0e34bfe0091d60b83eb3314aeab531e110bdb4
Nicolas Rivera <roberto.rivera.b@outlook.com>
2013-11-30 15:31:07 -0300

    guarddado para pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131123164826) do
+ActiveRecord::Schema.define(version: 20131123202646) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.string   "text"
+    t.string   "photo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
